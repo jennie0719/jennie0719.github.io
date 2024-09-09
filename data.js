47e8ed8cdc74ed7907e2fd8248d8072262009fe3
jennie0719 <jennie96179@gmail.com>
2024-09-08 18:38:18 -0700

    initial commit

---
+++ data.js
@@ -0,0 +1,195 @@
+export const bio = [
+  "Hi! I'm Jennie Lin. I'm currently pursuing a Master's degree in Computer Science at the Northeastern University, Seattle.",
+  "Through leading several system design projects, I have developed a strong interest in system design, solving problems and building solutions. I am passionate about implementing my skills in challenges and build a more efficient world.",
+  "Thanks for visiting!",
+];
+
+export const skills = [
+  {
+    title: "Languages",
+    skillName: "Pyhton, C++, C#, Java",
+    color: "1",
+    percentage: "80",
+  },
+  {
+    title: "Frameworks/Libraries",
+    skillName: "Numpy, Pandas",
+    color: "6",
+    percentage: "70",
+  },
+  {
+    title: "Design",
+    skillName: "Azure, MySQL, Microsoft SQL Server",
+    color: "4",
+    percentage: "70",
+  },
+  {
+    title: "Version Control",
+    skillName: "GitHub",
+    color: "7",
+    percentage: "80",
+  },
+  {
+    title: "Tools",
+    skillName: "Microsoft Office, Google Apps Script",
+    color: "3",
+    percentage: "80",
+  },
+   {
+     title: "Analytics",
+     skillName: "Tableau",
+     color: "5",
+     percentage: "80",
+   },
+   {
+     title: "IDE",
+     skillName: "VS Code",
+     color: "6",
+     percentage: "70",
+   },
+];
+
+export const projects = {
+  longhorn: [
+    {
+      projectName: "Longhorn Bank & Trust",
+      image: "images/longhorn.jpg",
+      summary:
+        "Built a C# based MVC bank website utilizing SQL Server Database hosted on Azure, allowing three types of users to perform basic bank website actions.",
+      preview: "https://github.com/jennie0719/Longhorn_Bank",
+      techStack: ["C#", "SQL Server Database", "Bootstrap", "Azure"],
+    },
+  ],
+  petMypet: [
+    {
+      projectName: "PetMyPet Hotel",
+      image: "images/petmypet.jpg",
+      summary:
+        "Created a pet hotel system with Python utilizing MySQL hosted on Oracle, allowing five types of users to perform basic hotel reservation system actions.",
+      preview: "https://github.com/jennie0719/PetMyPet_Hotel",
+      techStack: ["Python", "MySQL", "Oracle Database"],
+    },
+  ],
+  everClean: [
+    {
+      projectName: "Ever-Clean",
+      image: "images/cleaning.jpg",
+      summary:
+        "Built a cleaner company application system implemented with Java, allowing two types of users to perform basic ERP actions.",
+      preview: "https://github.com/jennie0719/Ever-Clean",
+      techStack: ["Java"],
+    },
+  ],
+  pricePrediction: [
+    {
+      projectName: "Used-Car Price Prediction Project",
+      image: "images/price_prediction.png",
+      summary:
+        "Implemented nine ML models in Python for used-car buyers to predict price, including decision tree, logistic regression, etc..",
+      preview: "https://github.com/jennie0719/Used-Car_Price_Prediction",
+      techStack: ["Python", "Numpy", "Pandas"],
+    },
+  ],
+};
+
+export const experience = [
+  {
+    title: "BIMFM Technology Co., Ltd.",
+    duration: "August 2023",
+    subtitle: "System Software Developer Intern",
+    details: ["Engineered C# backend APIs for the design diagrams and equipment data functionality of the Minsheng Sewerage Flow Project",
+      "Initiated an internal payroll and expense system for 50% efficiency enhancement",
+      "Designed database schema and UI/UX interface using Microsoft SQL Server and Figma to ensure seamless integration with the current management system",
+    ],
+    tags: ["C#", "Microsoft SQL Server", "Figma"],
+    icon: "tools ",
+  },
+  {
+    title: "AmazingTalker Ltd.",
+    duration: "July 2022",
+    subtitle: "Accounting Intern",
+    details: [
+      "Improved existing tools for preparation of financial reports with JavaScript",
+      "Collaborated in a team project and designed a new tool for CFO to monitor cash outflows",
+    ],
+    tags: [
+      "JavaScript",
+      "Google Apps Script",
+    ],
+    icon: "group",
+  },
+  {
+    title: "Good Finance Securities Co., Ltd.",
+    duration: "June 2021",
+    subtitle: "Accounting Intern",
+    details: [
+      "Participated in the preparation of journal entries and financial reports",
+      "Assisted with month-end financial report and reconciled quarter-end financial statements",
+    ],
+    icon: "glasses",
+  },
+];
+
+export const education = [
+  {
+    title: "MS, Computer Science",
+    duration: "2024-2026",
+    subtitle: "Northeastern University, Seattle",
+    details: [],
+    tags: [
+      "Programming Design Paradigms",
+      "Algorithms",
+    ],
+    icon: "graduation-cap",
+  },
+  {
+    title: "McCombs BBA Exchange Program",
+    duration: "2022",
+    subtitle: "University of Texas, Austin",
+    details: [],
+    tags: ["Web Application Development", "Introduction to Blockchain and Cryptocurrency"],
+    icon: "book",
+  },
+  {
+    title: "BBA, Professional Accounting and Information Systems",
+    duration: "2019-2023",
+    subtitle: "Hong Kong University of Science and Technology",
+    details: [],
+    tags: ["Information Systems Analysis and Design", 
+      "Database Design and Administration",
+      "Developing AI Applications",
+      "Internet Applications Development",
+      "Data Mining for Business Analytics",
+      "Web Analytics",
+      "Linear Algebra",
+      "Object-Oriented Programming and Data Structures",
+    ],
+    icon: "graduation-cap",
+  },
+];
+
+export const footer = [
+  {
+    label: "Thanks for visiting!",
+    data: [
+      {
+        text: "Email",
+        link: "jennie96179@gmail.com",
+      },
+      {
+        text: "Linkedin",
+        link: "https://www.linkedin.com/in/tzu-ching-lin-002040207/",
+      },
+      {
+        text: "Github/jennie0719",
+        link: "https://github.com/jennie0719",
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: [
+      "Made with &hearts; by Vinay Somawat.",
+    ],
+  },
+];
